bf307e12a7d0743beba8f59502b631a7e25f1c25
Ellie Liou <eliou1@hwemail.com>
2025-09-30 08:44:26 -0700

    (GP-2.2): Created a Hash Function
    Created SHA1Hash() utilize geeksforgeeks SHA1 code.

--- initializeGit.java
+++ initializeGit.java
@@ -1,11 +1,15 @@
 import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class initializeGit {
     public static void main(String[] args) throws IOException {
-        initialize();
-        System.out.println("done");
+        // initialize();
+        System.out.println("testing SHA1");
+        System.out.println(SHA1Hash("HELLO"));
     }
 
     public static void initialize() throws IOException {
@@ -30,6 +34,38 @@ public class initializeGit {
             index.createNewFile();
             HEAD.createNewFile();
             System.out.println("Git Repository Created");
+        }
+    }
+
+    // SHA1 Code taken from geeksforgeeks website
+    public static String SHA1Hash(String input) {
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+
+            // Add preceding 0s to make it 40 digits long
+            while (hashtext.length() < 40) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
     }
 }
